854642dc45d91f0a9be166390bc669551c87a3c7
FatemehRajabi <100088087+FatemehRajabi@users.noreply.github.com>
2025-05-11 19:48:46 -0400

    Configure static resource handler to serve uploaded images from /uploads directory

--- app/src/main/java/com/fatemeh/ecommerce/ecommerce/EcommerceApplication.java
+++ app/src/main/java/com/fatemeh/ecommerce/ecommerce/EcommerceApplication.java
@@ -3,6 +3,14 @@ package com.fatemeh.ecommerce.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Web configuration to expose the local /uploads directory as a static resource.
+ * This allows uploaded images to be accessed directly via URLs like:
+ * http://localhost:8080/uploads/filename.jpg
+ */
+
+
 @SpringBootApplication
 public class EcommerceApplication {
 
